c6131fc1edd15385e2bbb95920bf4eeb44b1770f
mickael-lhp5 <mickaelpierrefrancois@gmail.com>
2021-01-13 15:26:05 +0100

    premier commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+AOS.init({duration: 1200})
+
+
+
+
+
+// const burger = document.querySelector('.burger');
+
+// burger.addEventListener('click', () => {
+//     burger.classList.toggle('active');
+
+// });
---
+++ index.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <title>L'ANECDOTEtest</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/teststyle.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <!-- animation AOS.init img page d'accueil -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+<div>
+</div>
+
+  <nav class="navbar navbar-expand-lg">
+    <a href="#"><img src="assets/img/logoblanc.png" alt="logoblanc"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+    </button>
+    <div class="navbarMenu collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#lanecdote"><span style="color:white">L'ANECDOTE </span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#menus"> <span style="color:white"> MENUS</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#horaires"><span style="color:white">HORAIRES & INFORMATIONS</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact"><span style="color:white">NOUS CONTACTER</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#livre"><span style="color:white">LIVRE D'OR</span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <div id="test">
+    <div class="container">
+    <div class="row">
+      <div class="d-flex col-12 col-sm-4">
+        <div class="m-2" data-aos="zoom-in" data-aos-duration="1500">
+        
+          <img class="w-100 homeImg" src="assets/img/carousel1.jpg">
+        </div>
+      </div>
+      <div class="d-flex ol-12 col-sm-4">
+        <div class="m-2" data-aos="zoom-in" data-aos-duration="1500">
+          <img class="w-100  homeImg" src="assets/img/carousel2.jpg">
+        </div>
+      </div>
+      <div class="d-flex col-12 col-sm-4">
+        <div class="m-2" data-aos="zoom-in" data-aos-duration="1500">
+          <img class="w-100 homeImg" src="assets/img/IMG_20200723_215527.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+
+  </div>
+
+  <footer class="text-white text-lg-start">
+    <!-- Grid container -->
+    <div class="footerContainer p-4 ml-0 mr-0">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="">
+            <p><i class="fa fa-map-marker"></i> 5 rue Saint Louis 76600 Le Havre</p>
+          </div>
+        </div>
+        <!--Grid column-->
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <p><i class="fa fa-phone"></i> 09 83 87 57 14</p>
+        </div>
+
+        <!--Grid column-->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="">
+            <p><a href="#!" class="text-white">Mentions légales</a></p>
+          </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="d-flex justify-content-start justify-content-sm-end">
+            <a class="m-1" href="#!"><span style="color:white"><i class="fa fa-facebook-square"></i></span></a>
+            <a class="m-1" href="#!"><span style="color:white"><i class="fa fa-instagram"></i></i></span></a>
+            <a class="m-1" href="#!"><span style="color:white"><i class="fa fa-linkedin-square"></i></span></a>
+          </div>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+  </footer>
+
+  <!-- <nav class="myNav">
+    <div class="logo">
+      <img src="assets/img/logoblanc.png" alt="logoBlanc">
+    </div>
+    <ul class="navLinks">
+      <li>
+        <a href="#lanecdote"><span style="color:white">L'ANECDOTE </span></a>
+      </li>
+      <li>
+        <a href="#menus"> <span style="color:white"> MENUS</span></a>
+      </li>
+      <li>
+        <a href="#horaires"><span style="color:white">HORAIRES & INFORMATIONS</span></a>
+      </li>
+      <li>
+        <a href="#contact"><span style="color:white">NOUS CONTACTER</span></a>
+      </li>
+      <li>
+        <a href="#livre"><span style="color:white">LIVRE D'OR</span></a>
+      </li>
+    </ul>
+    <div class="burger">
+      <span></span>
+    </div>
+  </nav> -->
+
+
+  <!-- <div class="slideFrame">
+    <div class="slideImages">
+      <div class="imgContainer">
+        <img src="assets/img/carousel1.jpg" style="width:100%">
+      </div>
+      <div class="imgContainer">
+        <img src="assets/img/carousel2.jpg" style="width:100%">
+      </div>
+      <div class="imgContainer">
+        <img src="assets/img/carousel3.jpg" style="width:100%">
+      </div>
+
+    </div>
+
+    
+
+    <div style="text-align:center">
+      <span class="dot"></span>
+      <span class="dot"></span>
+      <span class="dot"></span>
+    </div>
+
+    <footer class="myFooter">
+
+      <div class="footerLeft">
+        <a href="#"><span style="color:white">L'ANECDOTE </span></a>
+        <p> 5 rue Saint Louis - 76600 Le Havre<p>
+      </div>
+
+      <div class="footerMiddle">
+        <a href=""><span style="color:white">Mentions légales</span></a>
+      </div>
+
+      <div class="footerRight">
+        <a href=""><span style="color:white"><i class="fa fa-facebook-square"></i></span></a>
+        <a href=""><span style="color:white"><i class="fa fa-instagram-square"></i></span></a>
+        <a href=""><span style="color:white"><i class="fa fa-linkedin-square"></i></span></a>
+      </div>
+
+    </footer> -->
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+  <script src="assets/js/script.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>AOS.init()</script>
+</body>
+
+
+
+</html>
